81dc17be2f4b8e07bffb5415f1c74b747d78f385
Michail <redredovich0@gmail.com>
2020-04-29 22:06:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+var red = 1;
+let second = 2;
+const pi = 3.14;
+
+let person = {
+    name: "John",
+    age: 25,
+    isMaried: false
+};
+
+// console.log(person.isMaried);
+
+// alert("Hello world!");
+
+// let answer = confirm("Are you here");
+
+// console.log(answer);
+
+let answer = prompt("Are you 18", "Yes");
+
+console.log(typeof(answer));
