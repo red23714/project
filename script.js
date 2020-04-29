181a45fdb4a13be2cde1d29abe834a7ce24e07cd
Michail <redredovich0@gmail.com>
2020-04-30 00:55:14 +0300

    Examplle

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-"use strict";
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-
-var red = 1;
-let second = 2;
-const pi = 3.14;
-
-let person = {
-    name: "John",
-    age: 25,
-    isMaried: false
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
 
-// console.log(person.isMaried);
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
-// alert("Hello world!");
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
-// let answer = confirm("Are you here");
-
-// console.log(answer);
-
-let answer = prompt("Are you 18", "Yes");
-
-console.log(typeof(answer));
+alert(appData.budget / 30);
