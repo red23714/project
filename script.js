c2232831dd6e59c266b130b6f39d8a6072dfcbf9
Michail <redredovich0@gmail.com>
2020-04-30 12:36:35 +0300

    Exampll

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ let appData = {
 	savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
+for(let i = 0; i < 2;i++){
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+		b = prompt("Во сколько обойдется?", '');
+		
+		if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+		&& a != '' && b != '' && a.length < 50){
+			console.log('done');
+			appData.expenses[a] = b;
+		} else 
+			i = 0;
+}
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-
+appData.moneyPerDay = alert(appData.budget / 30);
 alert(appData.budget / 30);
